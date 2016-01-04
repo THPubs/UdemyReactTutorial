4f7db2eb2d4dab42766a2840ce598249550678c5
Pubudu Kodikara <pubudu@leafycode.com>
2016-01-04 20:12:12 +0530

    Props and events complete

--- src/app.jsx
+++ src/app.jsx
@@ -1,26 +1,13 @@
 var React = require('react');
-var ThumbnailList = require('./thumbnail-list');
+var Dropdown = require('./dropdown');
 
 var options = {
-    thumbnailData: [
-        {
-            title: 'See Tutorials',
-            number: 32,
-            header: 'Learn React',
-            description: 'React is a fantastic new language to do cool stuff which we can now! Provide contextual feedback messages for typical user actions with the handful of available and flexible alert messages.',
-            imageUrl: 'https://raw.githubusercontent.com/wiki/facebook/react/react-logo-1000-transparent.png'
-        }, {
-            title: 'Electron Tutorials',
-            number: 52,
-            header: 'Learn Electron',
-            description: 'We would add deprecation warnings in X.Y releases. We would also add replacement APIs at the same time. X+1.0 would have the deprecated APIs removed. We could also add completely new APIs in minor versions',
-            imageUrl: 'https://avatars0.githubusercontent.com/u/6200624?v=3&s=400'
-        },
-    ]
+  title: 'Choose a desert', // What should show up on the button
+  items: ['Apple Pie', 'Peach Cobbler', 'Chocolate Lava Cake',]
 };
 
 // ask react to render this class
-var element = React.createElement(ThumbnailList, options);
+var element = React.createElement(Dropdown, options);
 
 // when we ask react to render, we tell it where to render
 ReactDOM.render(element, document.querySelector('.container'));
